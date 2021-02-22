68aa9e9ca2ed7c81e3eb84e9c1cc5414a8008328
m4nos <herman226.r@gmail.com>
2021-02-22 20:51:42 +0200

    implementing publisher-subscriber model

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,14 +6,6 @@ import recipeView from './views/recipeView.js';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -33,10 +25,12 @@ const controlRecipes = async function () {
     // render recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
