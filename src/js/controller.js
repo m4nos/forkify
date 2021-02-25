690d394d068c75ea0fefaf742043811b93fe5307
m4nos <herman226.r@gmail.com>
2021-02-25 16:17:07 +0200

    display page selection buttons

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 if (module.hot) {
   module.hot.accept();
@@ -29,12 +30,18 @@ const controlSearchResults = async function () {
   try {
     resultsView.renderSpinner();
 
+    // get search query
     const query = searchView.getQuery();
     if (!query) return;
 
+    // load search results
     await model.loadSearchResults(query);
 
-    resultsView.render(model.getSearchResultsPage(1));
+    //render results
+    resultsView.render(model.getSearchResultsPage(4));
+
+    // render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,57 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    console.log(numPages);
+
+    if (this._data.page === 1 && numPages > 1) {
+      return `
+      
+        <button class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+        </button>
+      `;
+    }
+
+    if (this._data.page === numPages && numPages > 1)
+      return `
+        
+            <button class="btn--inline pagination__btn--prev">
+                <svg class="search__icon">
+                    <use href="${icons}#icon-arrow-left"></use>
+                </svg>
+            <span>Page ${curPage - 1}</span>
+        </button>
+      `;
+
+    if (this._data.page < numPages)
+      return `
+        
+        <button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+        </button>
+        
+        <button class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+        </button>
+        `;
+  }
+}
+
+export default new PaginationView();
