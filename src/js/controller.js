c136ddb25e4a0f4850922ba8623c493e0067a019
m4nos <herman226.r@gmail.com>
2021-02-25 21:32:50 +0200

    implement pagination on search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -38,7 +38,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //render results
-    resultsView.render(model.getSearchResultsPage(4));
+    resultsView.render(model.getSearchResultsPage(1));
 
     // render initial pagination buttons
     paginationView.render(model.state.search);
@@ -47,9 +47,18 @@ const controlSearchResults = async function () {
   }
 };
 
+const controlPagination = goToPage => {
+  //render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 
 init();
